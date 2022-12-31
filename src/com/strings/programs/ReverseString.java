f0e08dfa6be197e113eac91dfcf9c1d5d3c6ffef
BhushanGhodke <bhushanghodke1500@gmail.com>
2022-12-31 22:25:32 +0530

    Reverse String

---
+++ src/com/strings/programs/ReverseString.java
@@ -0,0 +1,17 @@
+package com.strings.programs;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		String s="Java Developer";
+		char[]ch= s.toCharArray();
+		String a= "";
+		
+		for(int i=ch.length-1;i>=0;i--) {
+			a=a+ch[i];
+			
+		}
+		System.out.println("Reverse String is "+a);
+	}
+}
