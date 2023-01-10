c2cfff355eaf2cd64ff077ab084e91fdcde3eeeb
BhushanGhodke <bhushanghodke1500@gmail.com>
2023-01-10 22:40:13 +0530

    Work done

---
+++ src/com/strings/programs/OccurennceofEachCharacter.java
@@ -0,0 +1,17 @@
+package com.strings.programs;
+
+import java.util.Scanner;
+
+public class OccurennceofEachCharacter {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter any String");
+		Scanner scanner= new Scanner(System.in);
+		
+		String s= scanner.next().toLowerCase();
+		System.out.println(s);
+		
+		System.out.println(s);
+	}
+}
