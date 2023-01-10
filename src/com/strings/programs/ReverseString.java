125fce2bf3797ba059baaf85b058f4eaa648dcf9
BhushanGhodke <bhushanghodke1500@gmail.com>
2023-01-10 22:33:39 +0530

    String palindrome added

--- src/com/strings/programs/ReverseString.java
+++ src/com/strings/programs/ReverseString.java
@@ -1,17 +1,30 @@
 package com.strings.programs;
+
+import java.util.Scanner;
+
 //Write a program for Reverse String 
 public class ReverseString {
 
 	public static void main(String[] args) {
 		
-		String s="Java Developer";
+		System.out.println("Enter any String ");
+		Scanner scanner = new Scanner(System.in);
+		String s=scanner.next().toLowerCase();
 		char[]ch= s.toCharArray();
 		String a= "";
-		
 		for(int i=ch.length-1;i>=0;i--) {
 			a=a+ch[i];
 			
 		}
-		System.out.println("Reverse String is "+a);
+		
+		System.out.println("Reverse String is: "+a);
+	 
+		if(s.equals(a)) {
+			System.out.println("String is palindrome");
+		}
+		else {
+			System.out.println("String is not palindrome");
+		}
+	
 	}
 }
