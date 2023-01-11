7ca1678ca4903a377e84811edd0a55240ad80155
BhushanGhodke <bhushanghodke1500@gmail.com>
2023-01-11 13:09:30 +0530

    String immutable added

---
+++ src/com/strings/programs/String_immutable.java
@@ -0,0 +1,13 @@
+package com.strings.programs;
+
+public class String_immutable {
+
+	public static void main(String[] args) {
+		
+		String s= "Bhushan";
+
+		StringBuffer sb= new StringBuffer(s);
+		
+		System.out.println(sb+" Ghodke");
+	}
+}
